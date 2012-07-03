f314f5ec396809784c8a2035c66b72bb8951a4f3
Jun Zhao <jun.zhao@zoo.ox.ac.uk>
2012-07-03 12:27:41 +0100

    added replicable test and cleaned up

--- v0.1/me-pack-55/minim-checklist.sh
+++ v0.1/me-pack-55/minim-checklist.sh
@@ -10,11 +10,11 @@ TESTRO="$ROPATH/$RONAME"
 
 echo "--------"
 
-$RO evaluate checklist -d $TESTRO -a me-pack-55-minim.rdf "Runnable" $TESTRO/
+$RO evaluate checklist -d $TESTRO -a me-pack-55-runnable.rdf "Runnable" $TESTRO/
 
 echo "--------"
 
-#$RO evaluate checklist -v -d $TESTRO -a me-pack-55-minim.rdf "Reviewable" $TESTRO/
+$RO evaluate checklist -d $TESTRO -a me-pack-55-replicable.rdf "Replicable" $TESTRO/
 
 echo "--------"
 
