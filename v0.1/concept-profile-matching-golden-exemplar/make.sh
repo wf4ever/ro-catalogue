8fc41cafa8c4a543c910a667c6acd4f470c3433b
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-07-31 11:09:58 +0100

    wfdesc annotations and typing of datasets/documents/runs

--- v0.1/concept-profile-matching-golden-exemplar/make.sh
+++ v0.1/concept-profile-matching-golden-exemplar/make.sh
@@ -3,7 +3,10 @@
 # RO manager script to create trivial RO
 #
 
-source ../../ro.sh.kmh-kmhettne
+source ../../ro.sh
+
+# Fail on first error
+set -e
 
 #Should not be needed if above shell is run
 #ROBASE=~/Projects/ro-catalogue/v0.1 
@@ -32,12 +35,41 @@ $RO add -v -a -d .
 $RO status -v -d .
 
 $RO list -v -d .
-
 echo "--------"
 
 $RO list -v -a -d .
+echo "--------"
+
+# Make wfdesc from t2flow
+pushd Workflows
+    ../../../lib/scufl2-wfdesc/bin/scufl2-to-wfdesc *t2flow 
+    # Move away, we don't want to aggregate these annotation bodies
+    # as ro:Resources
+    mv *wfdesc.ttl ../.ro/
+    for t2flow in *t2flow ; do
+        $RO annotate -v $t2flow -g ../.ro/$(echo $t2flow|sed s/.t2flow$/.wfdesc.ttl/)
+        $RO annotate -v $t2flow rdf:type 'http://purl.org/wf4ever/wfdesc#Workflow'
+    done
+popd
 
 echo "--------"
+pushd Datasets
+    for data in * ; do
+        $RO annotate -v $data rdf:type 'http://purl.org/wf4ever/wf4ever#Dataset'
+    done
+popd
+echo "--------"
+pushd Documents
+    for doc in * ; do
+        $RO annotate -v $doc rdf:type 'http://purl.org/wf4ever/wf4ever#Document'
+    done
+popd
+echo "--------"
+pushd Workflow-runs
+    for run in *run*rdf ; do
+        $RO annotate -v $run rdf:type 'http://purl.org/wf4ever/wfprov#WorkflowRun'
+    done
+popd
 
 #$RO annotate -v showcase62a_wf-withMulitipleBPCUIs.t2flow title "Anni golden exemplar main workflow"
 #$RO annotate -v showcase62a_wf-withMulitipleBPCUIs.t2flow title "Anni golden exemplar main workflow"
