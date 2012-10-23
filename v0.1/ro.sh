b6e81fca2df32963b3a0d86114c59d6dbbe74c53
Graham Klyne <gk-github@ninebynine.org>
2012-10-23 17:58:16 +0100

    Resolve symlinks in RO base URI so that relative URIs are recognized consistently

--- v0.1/ro.sh
+++ v0.1/ro.sh
@@ -3,6 +3,12 @@
 # Definitions to run RO manager
 #
 
+# Resolve any symlinks in $ROBASE
+
+pushd $ROBASE
+ROBASE=`pwd -P`
+popd
+
 ROPATH=$ROBASE/v0.1
 ROSRS_URI="http://sandbox.wf4ever-project.org/rodl/ROs/"
 ROSRS_ACCESS_TOKEN="32801fc0-1df1-4e34-b"
