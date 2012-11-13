03c784ab99bbc5697d4e938df2e9a1744dce1cbc
Graham Klyne <gk-github@ninebynine.org>
2012-11-13 17:03:54 +0000

    Add Hello World workflow, remove some RO metadata

---
+++ v0.1/HelloWorld/README.txt
@@ -0,0 +1,15 @@
+Hello World Research Object
+
+This is a simple Research Object that embeds a simple "Hello World" workflow.
+
+To run this workflow in Taverna, first edit the fileurl_value and outputFile_value to reflect paths on the local system to the Research Object directory.
+
+Research Object files are:
+
+README.txt - this file
+make.sh - sample script to create Research Object structure using RO Manager
+TavernaHelloWorld.t2flow - Taverna "Hello world" workflow
+InputName.txt - sample input file
+HelloOutput.txt - result from running workflow with sample input file
+HelloWorld-wfdesc.rdf - Workflow description using WFDesc vocabulary terms (http://wf4ever.github.com/ro/)
+
---
+++ v0.1/HelloWorld/make.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# RO manager script to create trivial RO
+#
+
+source ../../ro.sh
+
+TESTRO="."
+RONAME="hello-world"
+
+echo "--------"
+
+$RO config -v \
+  -b $ROPATH \
+  -r $ROSRS_URI \
+  -t "$ROSRS_ACCESS_TOKEN" \
+  -n "Test user" \
+  -e "testuser@example.org"
+
+# ro config \
+#    -b /Users/graham/Desktop/robase \
+#    -r "http://sandbox.wf4ever-project.org/rodl/ROs/" \
+#    -t "32801fc0-1df1-4e34-b" \
+#    -n "Test user" \
+#    -e "testuser@example.org"
+
+#  -b /Users/graham/Desktop/robase \                     # base directory for ROs
+#  -r "http://sandbox.wf4ever-project.org/rodl/ROs/" \   # URI for ROSRS service used for ro push, checkout
+#  -t "32801fc0-1df1-4e34-b" \                           # Auth bearer token for ROSRS access
+#  -n "Test user" \                                      # User name
+#  -e "testuser@example.org"                             # User email
+
+# show simple directory content
+
+ro create "Hello World" -d . -i hello-world
+
+# add all files from currenmt directory to RO
+
+ro add -a -d .
+
+# Show aggregated files
+
+ro list -d .
+
+# Create new single annotation with explicit target
+
+ro annotate TavernaHelloWorld.t2flow title "Hellow World workflow"
+
+# Display new annotation on filke
+
+ro annotations TavernaHelloWorld.t2flow
+
+# Also show nbew files created in RO directory
+
+# Add nore annotations using pre-existing RDF file
+
+ro annotate TavernaHelloWorld.t2flow -g HelloWorld-wfdesc.rdf
+
+# Show all annotations
+
+ro annotations
+
+# End.
+
+
