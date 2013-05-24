833babc18f0463c072f649002443d010159e5160
Graham Klyne <gk-github@ninebynine.org>
2013-05-24 16:46:40 +0100

    Update access tokens for RODL

--- v0.1/ro.sh
+++ v0.1/ro.sh
@@ -12,5 +12,7 @@ popd
 ROPATH=$ROBASE/v0.1
 ROSRS_URI="http://sandbox.wf4ever-project.org/rodl/ROs/"
 ROSRS_ACCESS_TOKEN="4d0cf8b6-0d4c-4150-9f29-0e2b9081ed18"
+# Wf4Ever test user:
+ROSRS_ACCESS_TOKEN="972850de-1d98-488d-bd34-a19c9b86eb76"
 
 # End.
