3bda12cd90e0b414ac192632e2b0e28e6fec282b
Graham Klyne <gk-github@ninebynine.org>
2013-05-28 14:59:20 +0100

    Trying to get chembox evaluation working against RODL - so far, failing

--- v0.1/in-use-submission/chemin-box/README.txt
+++ v0.1/in-use-submission/chemin-box/README.txt
@@ -2,9 +2,11 @@ chembox evaluation experiment
 
 Converting a small part of Matthew Gamble's "chemmim" evaluation of chemical descriptions to use the RO checklist evaluation framework, with a view to making a concrete comparison between the approaches used.
 
+@@NOTE:  at the present time (28-May-2013) I am having problems running this evaluation against the chembox data in sandbox.wf4ever-project.org/rodl, due to a problem uploading and/or retrieving the RO.  The tests have been performed using a local instance of the RO checklist service, which requires installing the RO Manager and Pyramid web framework software to the local computer.
+
 == Content of the folder ==
 
-chembox_evaluate.sh: the script for running the assessment locally or remotedly
+chembox_evaluate.sh: the script for running the assessment locally or remotely
 
 chembox-minim-samples.ttl: the checklist used for assessment
 
@@ -16,4 +18,8 @@ MIM-chembox-uris.100.results: the assessment result from the MIM assessment tool
 
 == To reproduce the Minim assessment ==
 
-./chembox_evaluate.sh
+    ./chembox_evaluate.sh
+
+Expected results:
+
+
--- v0.1/in-use-submission/chemin-box/chembox_evaluate.sh
+++ v0.1/in-use-submission/chemin-box/chembox_evaluate.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 #
 
-LOCAL=NO
-#if [ "$LOCAL"=="YES" ]; then
-#  CHECKLIST_SERVICE=http://localhost:8080/evaluate/trafficlight_json
-#  ROBASE=file:///usr/workspace/wf4ever-ro-catalogue/v0.1
-#  echo "Using local evaluation of $ROBASE"
-#else
-CHECKLIST_SERVICE=http://sandbox.wf4ever-project.org/roevaluate/evaluate/trafficlight_json
-ROBASE=http://sandbox.wf4ever-project.org/rodl/ROs
-#âÃfi
+USE_LOCAL="NO"
+
+if [ "$USE_LOCAL" == "YES" ]; then
+  CHECKLIST_SERVICE=http://localhost:8080/evaluate/trafficlight_json
+  ROBASE=file:///usr/workspace/wf4ever-ro-catalogue/v0.1
+  echo "Using local evaluation of $ROBASE (USE_LOCAL: $USE_LOCAL)"
+fi
+
+if [ "$USE_LOCAL" == "NO" ]; then
+  CHECKLIST_SERVICE=http://sandbox.wf4ever-project.org/roevaluate/evaluate/trafficlight_json
+  ROBASE=http://sandbox.wf4ever-project.org/rodl/ROs
+  echo "Using sandbox evaluation of $ROBASE (USE_LOCAL: $USE_LOCAL)"
+fi
+
+echo "CHECKLIST_SERVICE: $CHECKLIST_SERVICE"
+echo "ROBASE:            $ROBASE"
+
 ROSRS_ACCESS_TOKEN="4d0cf8b6-0d4c-4150-9f29-0e2b9081ed18"
-RONAME=minim-evaluation
+RONAME=minim-evaluation-1
 ROURI=$ROBASE/$RONAME
 CHECKLIST_URI=$ROBASE/$RONAME/chembox-minim-samples.ttl
 
@@ -22,13 +30,16 @@ countall=0
 while read TARGET
   do
     echo "----- $ROURI :: $TARGET -----"
-    echo "RO evaluation run: $(date)" >00-evaluate-ROs
+    echo "RO evaluation run: $(date)" >00-evaluate-ROs.log
 
     if [ "${OPT:0:4}" != "skip" ]; then
       # checklist eval ...
-      result=$(curl --silent \
-            "$CHECKLIST_SERVICE?RO=$ROURI&minim=$CHECKLIST_URI&purpose=complete&target=$TARGET" \
-          | python checklistresult.py)
+      # result=$(curl --silent \
+      #       "$CHECKLIST_SERVICE?RO=$ROURI&minim=$CHECKLIST_URI&purpose=complete&target=$TARGET" \
+      #     | python checklistresult.py)
+      curl "$CHECKLIST_SERVICE?RO=$ROURI&minim=$CHECKLIST_URI&purpose=complete&target=$TARGET" \
+           >00-checklistresult.tmp
+      result=$(cat 00-checklistresult.tmp | python checklistresult.py)
       # @@TODO: count results for individual checklist items
       # (or, just hack the 
       if [ $? == 0 ]; then
@@ -42,8 +53,8 @@ while read TARGET
 
   # done <chembox-uris-tryptoline.txt
   # done <chembox-uris-dihydrothiazole.txt
-  # done <chembox-uris-test.txt
-  done <chembox-uris-100.txt
+  done <chembox-uris-test.txt
+  # done <chembox-uris-100.txt
 
 echo "Total pass: $countpass, fail: $countfail out of $countall"
 
