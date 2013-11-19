cecbfb75b7b8d668b88968ac6a1b0f796c7826af
Graham Klyne <gk-github@ninebynine.org>
2013-11-19 16:34:14 +0000

    Update script to build enhanced checklist using new reporting capabilities

--- v0.1/Y3demo-test/makepushminim.sh
+++ v0.1/Y3demo-test/makepushminim.sh
@@ -6,9 +6,15 @@
 mkminim -o turtle Y3demo-minim.xls >Y3demo-minim.ttl
 mkminim -o xml Y3demo-minim.xls >Y3demo-minim.rdf
 git add Y3demo-minim.xls Y3demo-minim.ttl Y3demo-minim.rdf
+
+mkminim -o turtle Y3demo-minim-enhanced.xls >Y3demo-minim-enhanced.ttl
+mkminim -o xml Y3demo-minim-enhanced.xls >Y3demo-minim-enhanced.rdf
+git add Y3demo-minim-enhanced.xls Y3demo-minim-enhanced.ttl Y3demo-minim-enhanced.rdf
+
 git commit -m "Update Y3demo minim checklist"
 git checkout gh-pages
 git merge master
 git checkout master
 git status
+
 echo "'git push' to publish in github"
