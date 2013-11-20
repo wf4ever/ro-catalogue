a8cece387e6e38e3c252fb3b70cd856cb89ce88a
Graham Klyne <gk-github@ninebynine.org>
2013-11-20 09:26:59 +0000

    Update Y3demo minim checklist

--- v0.1/Y3demo-test/makeminim.sh
+++ v0.1/Y3demo-test/makeminim.sh
@@ -3,10 +3,12 @@
 # Regenerate and commit updated Y3 demo checklist
 #
 
+echo "Building Y3demo-minim.ttl, Y3demo-minim.rdf ..."
 mkminim -o turtle Y3demo-minim.xls >Y3demo-minim.ttl
 mkminim -o xml Y3demo-minim.xls >Y3demo-minim.rdf
 git add Y3demo-minim.xls Y3demo-minim.ttl Y3demo-minim.rdf
 
+echo "Building Y3demo-minim-enhanced.ttl, Y3demo-minim-enhanced.rdf ..."
 mkminim -o turtle Y3demo-minim-enhanced.xls >Y3demo-minim-enhanced.ttl
 mkminim -o xml Y3demo-minim-enhanced.xls >Y3demo-minim-enhanced.rdf
 git add Y3demo-minim-enhanced.xls Y3demo-minim-enhanced.ttl Y3demo-minim-enhanced.rdf
@@ -15,6 +17,7 @@ git add Y3demo-minim-enhanced.xls Y3demo-minim-enhanced.ttl Y3demo-minim-enhance
 # git checkout gh-pages
 # git merge master
 # git checkout master
-# git status
 
-echo "'git push' to publish in github"
+echo "New minim files created"
+
+git status
